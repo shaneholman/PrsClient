3c2f9a930f9b7544f78c8fe988f89fec0199f8e0
Shane Holman <Sholman@tql.com>
2024-08-23 15:34:10 -0400

    final

--- src/Header.tsx
+++ src/Header.tsx
@@ -16,14 +16,14 @@ function Header() {
     <header className="container-fluid d-flex justify-content-between bg-body-tertiary">
       <span>
         <Link to="/">
-         <svg id="logo" width="50" height="39" viewBox="0 0 50 39" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z" className="ccompli1" fill="#007AFF"></path>
-          <path
-            d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
-            className="ccustom"
-            fill="#312ECB"
-          ></path>
-        </svg>
+          <svg id="logo" width="50" height="39" viewBox="0 0 50 39" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z" className="ccompli1" fill="#007AFF"></path>
+            <path
+              d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
+              className="ccustom"
+              fill="#312ECB"
+            ></path>
+          </svg>
         </Link>
         <span className="brand">Purchase Request System</span>
       </span>
@@ -38,10 +38,7 @@ function Header() {
           {user?.firstname} {user?.lastname}
         </summary>
         <div className="d-flex justify-content-end">
-          <ul
-            className="dropdown-menu bg-body-tertiary"
-            style={{ display: "revert" }}
-          >
+          <ul className="dropdown-menu bg-body-tertiary" style={{ display: "revert" }}>
             <li>
               <a className="dropdown-item" href="#">
                 Profile
